2f68f1baf79e239af3b6c59bef332c819134001c
ymadz <138951434+ymadz@users.noreply.github.com>
2025-02-09 00:02:26 +0800

    added features
    - navbar
    - login functionality
    - debug toolbar

--- crm/urls.py
+++ crm/urls.py
@@ -15,8 +15,18 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from django.conf import settings  # Import settings
+from django.conf.urls.static import static  # For static files (optional)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include('website.urls')),
 ]
+
+# Include Debug Toolbar URLs when in DEBUG mode
+if settings.DEBUG:
+    import debug_toolbar
+    urlpatterns += [
+        path('__debug__/', include(debug_toolbar.urls)),
+    ]
